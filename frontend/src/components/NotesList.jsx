88b4de2d03fab5911eb36a5d86b36f4e44e5fa1b
Vishnu Kaushik <vishnu@Vishnus-MacBook-Pro.local>
2025-01-17 21:38:55 +0530

    format code and fix add-note button position

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -3,48 +3,46 @@ import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import { IconButton } from "@mui/material";
 import "../styles/NotesPage.css";
 
-
 const NotesList = ({
-  notes,
-  activeId,
-  handleNoteItemClickWrapper,
-  handleAdd,
+	notes,
+	activeId,
+	handleNoteItemClickWrapper,
+	handleAdd,
 }) => {
-  return (
-    <div
-      style={{
-        position: "relative",
-        height: "inherit",
-      }}
-    >
-      {notes.map((note, index) => {
-        return (
-          <NoteItem
-            key={index}
-            index={index}
-            note={note}
-            activeId={activeId === note._id?true:false}
-            handleNoteItemClick={handleNoteItemClickWrapper(index + 1, note)}
-          />
-        );
-      })}
-      <IconButton
-        sx={{
-          marginRight: "10px",
-          position: "absolute",
-          bottom: "20px",
-          right: "5px",
-        }}
-        onClick={handleAdd}
-      >
-        <NoteAddIcon
-          sx={{
-            color: "#2563eb",
-            fontSize: 30,
-          }}
-        />
-      </IconButton>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				position: "relative",
+				height: "inherit",
+			}}
+		>
+			{notes.map((note, index) => {
+				return (
+					<NoteItem
+						key={index}
+						index={index}
+						note={note}
+						activeId={activeId === note._id ? true : false}
+						handleNoteItemClick={handleNoteItemClickWrapper(index + 1, note)}
+					/>
+				);
+			})}
+			<IconButton
+				sx={{
+					position: "absolute",
+					bottom: "20px",
+					right: "5px",
+				}}
+				onClick={handleAdd}
+			>
+				<NoteAddIcon
+					sx={{
+						color: "#2563eb",
+						fontSize: 30,
+					}}
+				/>
+			</IconButton>
+		</div>
+	);
 };
 export default NotesList;
