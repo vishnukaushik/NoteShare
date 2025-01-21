7720b5b60518f1d09397c6c0739b2a98d7fad0c1
Vishnu Kaushik <vishnu@Vishnus-MacBook-Pro.local>
2025-01-21 22:34:17 +0530

    use bcryptjs instead of bcrypt package

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const axios = require("axios");
 
